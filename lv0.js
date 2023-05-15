08bc713f4ef88101b2f46adc4662bbca26a2161a
trung.dinh <trung.dinh@ntq-solution.com.vn>
2023-05-15 17:01:11 +0700

    update

--- lv0.js
+++ lv0.js
@@ -1,12 +1,12 @@
 setTimeout(function () {
     console.log('1');
-}); // macro-task
+}); // macro-task 1 chạy cuối
 
 new Promise(function (resolve, reject) {
     console.log('2'); // micro-task 1 execute
-    resolve(3); // micro-task 2
+    resolve(3);
 }).then(function (val) {
-    console.log(val);
+    console.log(val); // micro-task 2 execute
 });
 
 console.log('4'); // code tổng thể
--- lv1.js
+++ lv1.js
@@ -2,25 +2,29 @@ console.log('0'); // code tổng thể
 
 setTimeout(function () {
     console.log('1');
-}); // macro-task
+}); // macro-task 1 chạy cuối
 
 new Promise(function (resolve, reject) {
-    console.log('2'); // execute
-    resolve(3); // micro-task
+    console.log('2'); // micro-task 1 execute
+    resolve(3);
 }).then(function (val) {
-    console.log(val);
+    console.log(val); // micro-task 3 execute
 }).then(function () {
-    console.log('then...1');
+    console.log('then...1'); // micro-task 5 execute
 });
 
 new Promise(function (resolve, reject) {
-    console.log('4'); // execute
-    resolve(5); // micro-task
+    console.log('4'); // micro-task 2 execute
+    resolve(5);
 }).then(function (val) {
-    console.log(val);
+    console.log(val); // micro-task 4 execute
 }).then(function () {
-    console.log('then...2');
+    console.log('then...2'); // micro-task 6 execute
 });
+
+setTimeout(function () {
+    console.log('7');
+}); // macro-task 2 chạy cuối
 
 console.log('6'); // code tổng thể
 
@@ -35,4 +39,5 @@ console.log('6'); // code tổng thể
     then...1
     then...2
     1
+    7
 */
